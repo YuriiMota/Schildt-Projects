e40fdeaa9cdb7362dee83bdd35a635b0c149215a
Yurii <45452491+YuriiMota@users.noreply.github.com>
2018-12-08 16:55:17 +0200

    Add files via upload

---
+++ The Basics of Java Programming/src/Thread/Thread_2.java
@@ -0,0 +1,25 @@
+package Thread;
+class JThread extends Thread{
+    JThread(String name){
+        super(name);
+    }
+    public void run(){
+
+        System.out.printf("%s started... \n", Thread.currentThread().getName());
+        try{
+            Thread.sleep(500);
+        }
+        catch(InterruptedException e){
+            System.out.println("Thread has been interrupted");
+        }
+        System.out.printf("%s fiished... \n", Thread.currentThread().getName());
+    }
+}
+public class Thread_2 {
+    public static void main(String[] args) {
+
+        System.out.println("Main thread started...");
+        new JThread("JThread").start();
+        System.out.println("Main thread finished...");
+    }
+}
---
+++ The Basics of Java Programming/src/Thread/Thread_3_Runnable.java
@@ -0,0 +1,28 @@
+package Thread;
+
+class MyThread implements Runnable {
+
+
+    public void run(){
+
+        System.out.printf("%s started... \n", Thread.currentThread().getName());
+        try{
+            Thread.sleep(500);
+        }
+        catch(InterruptedException e){
+            System.out.println("Thread has been interrupted");
+        }
+        System.out.printf("%s finished... \n", Thread.currentThread().getName());
+    }
+}
+
+public class Thread_3_Runnable {
+
+    public static void main(String[] args) {
+
+        System.out.println("Main thread started...");
+        Thread myThread = new Thread(new MyThread(),"MyThread");
+        myThread.start();
+        System.out.println("Main thread finished...");
+    }
+}
